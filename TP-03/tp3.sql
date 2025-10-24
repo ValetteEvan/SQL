e33d65631aa1b93f870aa93a64b9d03b9ce457cc
Valette Evan <113213514+ValetteEvan@users.noreply.github.com>
2025-10-24 10:28:39 +0200

    Revert "TP-03 - Update SQL queries and add creation_compta2.sql"
    This reverts commit 91b5c54830af730396d240196d469f5ae1971dce.

--- TP-03/tp3.sql
+++ TP-03/tp3.sql
@@ -3,57 +3,43 @@
 
 USE compta2;
 
--- a. Listez toutes les données concernant les articles
-SELECT * FROM article;
 
--- b. Listez uniquement les références et désignations des articles de plus de 2 euros
-SELECT ref, designation
-FROM article
-WHERE prix > 2;
+SELECT * FROM ARTICLE;
 
--- c. En utilisant les opérateurs de comparaison, listez tous les articles dont le prix est
---    compris entre 2 et 6.25 euros
+SELECT REF, DESIGNATION
+FROM ARTICLE
+WHERE PRIX > 2;
+
 SELECT *
-FROM article
-WHERE prix >= 2 AND prix <= 6.25;
+FROM ARTICLE
+WHERE PRIX >= 2 AND PRIX <= 6.25;
 
--- d. En utilisant l'opérateur BETWEEN, listez tous les articles dont le prix est compris
---    entre 2 et 6.25 euros
 SELECT *
-FROM article
-WHERE prix BETWEEN 2 AND 6.25;
+FROM ARTICLE
+WHERE PRIX BETWEEN 2 AND 6.25;
 
--- e. Listez tous les articles, dans l'ordre des prix descendants, et dont le prix n'est pas
---    compris entre 2 et 6.25 euros et dont le fournisseur est Française d'Imports.
-SELECT a.*
-FROM article a
-JOIN fournisseur f ON a.id_fou = f.id
-WHERE (prix NOT BETWEEN 2 AND 6.25)
-  AND f.nom = 'Française d''imports'
-ORDER BY prix DESC;
+SELECT A.*
+FROM ARTICLE A
+JOIN FOURNISSEUR F ON A.ID_FOU = F.ID
+WHERE (PRIX NOT BETWEEN 2 AND 6.25)
+  AND F.NOM = 'Française d''Imports'
+ORDER BY PRIX DESC;
 
--- f. En utilisant un opérateur logique, listez tous les articles dont les fournisseurs sont la
---    Française d'imports ou Dubois et Fils
-SELECT a.*
-FROM article a
-JOIN fournisseur f ON a.id_fou = f.id
-WHERE f.nom = 'Française d''imports' OR f.nom = 'Dubois & Fils';
+SELECT A.*
+FROM ARTICLE A
+JOIN FOURNISSEUR F ON A.ID_FOU = F.ID
+WHERE F.NOM = 'Française d''Imports' OR F.NOM = 'Dubois & Fils';
 
--- g. En utilisant l'opérateur IN, réalisez la même requête que précédemment
-SELECT a.*
-FROM article a
-JOIN fournisseur f ON a.id_fou = f.id
-WHERE f.nom IN ('Française d''imports', 'Dubois & Fils');
+SELECT A.*
+FROM ARTICLE A
+JOIN FOURNISSEUR F ON A.ID_FOU = F.ID
+WHERE F.NOM IN ('Française d''Imports', 'Dubois & Fils');
 
--- h. En utilisant les opérateurs NOT et IN, listez tous les articles dont les fournisseurs ne
---    sont ni Française d'Imports, ni Dubois et Fils.
-SELECT a.*
-FROM article a
-JOIN fournisseur f ON a.id_fou = f.id
-WHERE f.nom NOT IN ('Française d''imports', 'Dubois & Fils');
+SELECT A.*
+FROM ARTICLE A
+JOIN FOURNISSEUR F ON A.ID_FOU = F.ID
+WHERE F.NOM NOT IN ('Française d''Imports', 'Dubois & Fils');
 
--- i. Listez tous les bons de commande dont la date de commande est entre le
---    01/02/2019 et le 30/04/2019.
 SELECT *
-FROM bon
-WHERE date_cmde BETWEEN '2019-02-01' AND '2019-04-30';
+FROM BON
+WHERE datetime BETWEEN '2019-02-01' AND '2019-04-30';
